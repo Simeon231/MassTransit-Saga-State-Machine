3e370a7e154394aea431e13b97461f0848a9ac0a
Simeon Kirilov <simeonkirilov1999@gmail.com>
2025-05-30 16:57:08 +0300

    Remove not used files

--- src/MasterDegreeDemo.AppHost/Program.cs
+++
@@ -1,8 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-builder
-    .AddProject<Projects.MasterDegreeDemo_EventReceiver1>("eventreceiver1");
-builder
-    .AddProject<Projects.MasterDegreeDemo_EventReceiver2>("eventreceiver2");
-
-builder.Build().Run();
